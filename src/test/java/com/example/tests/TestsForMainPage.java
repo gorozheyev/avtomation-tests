0674d1376636db5efc2252dd08d248502e81da8c
Dima <dgorozheyev@gmail.com>
2016-12-07 17:42:57 +0200

    Begin first test

---
+++ src/test/java/com/example/tests/TestsForMainPage.java
@@ -0,0 +1,80 @@
+/**
+ * Created by gorozheyevd on 07.12.2016.
+ */
+package com.example.tests;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class TestsForMainPage {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        baseUrl = "http://www.avtopoisk.ru/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testUntitled() throws Exception {
+        driver.get(baseUrl + "/");
+        driver.findElement(By.xpath("(//input[@type='text'])[2]")).click();
+        driver.findElement(By.xpath("(//input[@type='text'])[2]")).clear();
+        driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys("toyota");
+        driver.findElement(By.xpath("//input[@value='Найти']")).click();
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
